b07fe63f65677d25b74e5fec0f30ae1fdbe25bf1
craig <c3kimball@student.bridgew.edu>
2022-11-11 14:45:08 -0500

    Implementing a working, but very bare bones, shell.

--- shell.c
+++ shell.c
@@ -11,7 +11,17 @@ void main()
 
         syscall(0, "SHELL> ",0,0);
         syscall(1, commandInput,0,0);
-        syscall(0, commandInput, 0, 0);
+
+        // checking if the user wants to use the type command
+        // this if statement is a little scuffed, but it'll work
+        if (commandInput[0] == 't' && commandInput[1] == 'y' && commandInput[2] == 'p' && commandInput[3] == 'e')
+        {
+            syscall(0, "You want to type out a file");
+        }
+        else
+        {
+            syscall(0, "Error: not a valid command.");
+        }
 
         //Choose command to run
 
